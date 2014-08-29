8e33cca7732f621fea6f76fc8f7bce74ed554642
nukkunaana <nuochi@yandex.ru>
2014-08-29 19:25:56 +0400

    edit dt_click.js , create new class="yellow-txt"

--- js/dt_click.js
+++ js/dt_click.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 	// показывает и скрывает ответ на соответствующий вопрос
-	$('.select').click(function(){
+
+	$(".select").click(function(event){
+		//при клике на вопросы,сворачиваем все ответы
+		$("dd").slideUp(200);
 		// получаем атрибут у выбранного вопроса
 		var question = $(this).data("question");
 		// получаем ответ с таким же атрибутом
-		var answer= $("dd[data-answer=" + question+ "]");
-		// показывает или скрывает ответ
+		var answer = $("dd[data-answer=" + question + "]");
+		// показываем выбранный ответ
 		answer.slideToggle(200);
 	});
+		
 	// при клике на ответ, сворачивается
 	$('dd').click(function(){
 		$(this).slideUp(200);
-	});
-});
+	});	
+}); 
+
+
